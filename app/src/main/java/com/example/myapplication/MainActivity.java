97677a7d22b5c839ebd31bff90d58844b14a32dc
Davis <davis.kazemaks@gmail.com>
2023-05-05 16:16:38 +0200

    making pop ups for recognizing and labeling

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Components of the layout
+        Button button_recognize = (Button) findViewById(R.id.button_recognize);
+        Button button_label_room = (Button) findViewById(R.id.button_label_room);
+        Button button_top_5 = (Button) findViewById(R.id.button_top_5);
+        TextView list_of_rooms = (TextView) findViewById(R.id.list_of_rooms);
+
+        // Code for recognizing the current room
+        button_recognize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: ADD CODE FOR CLASSIFYING ROOM HERE
+
+                String room_label = "room label";
+
+                // Make a pop up showing the room label
+                Intent intent = new Intent(MainActivity.this, Pop.class);
+                intent.putExtra("title", "The room label is:");
+                intent.putExtra("text", room_label);
+                startActivity(intent);
+            }
+        });
+
+        // Code for labeling the current room
+        button_label_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Make a pop up showing the room label
+                Intent intent = new Intent(MainActivity.this, LabelWindow.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
