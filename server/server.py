43a339370c42c679fc84c2f9cd85fbc4e8a3cfe1
jmchan <J.M.Chan@student.tudelft.nl>
2023-05-07 23:38:13 +0200

    Added database

--- server/server.py
+++ server/server.py
@@ -1,14 +1,27 @@
 from room import Room
 from uuid import uuid4
 from flask import Flask, request 
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 APP = Flask(__name__)
 UNSAVED_ROOMS = {"1234": Room(None)}
+
+cred = credentials.Certificate('key.json')
+
+app = firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 
 @APP.route('/get_rooms', methods=['GET'])
 def get_rooms():
     #TODO: quesry database for all rooms
     room_list = []
+    collections = db.collections()
+    for collection in collections:
+        for document in collection.list_documents():
+            room_list.append(document.get().to_dict())
     return room_list
 
 @APP.route('/add_room', methods=['POST', 'GET'])
@@ -19,6 +32,8 @@ def add_room():
         room = Room(audio)
         room_uuid = uuid4()
         UNSAVED_ROOMS.update({room_uuid: room})
+
+
         return room_uuid
 
     if request.method == 'GET':
@@ -26,12 +41,22 @@ def add_room():
         room_uuid = room_data['uuid']
         room_label = room_data['room_label']
         building_label = room_data['building_label']
-        room = UNSAVED_ROOMS[room_uuid]
-        room.set_room_label(room_label)
-        room.set_building_label(building_label)
+        #room = UNSAVED_ROOMS[room_uuid]
+        #room.set_room_label(room_label)
+        #room.set_building_label(building_label)
 
         #TODO: save new room to the database
-        UNSAVED_ROOMS.pop(room_uuid)
+        data = {
+            u'building': building_label,
+            u'room': room_label,
+            u'audio': "Work in progress, will probably be a URL to firebase storage OR audio file encoding",
+            u'uuid':room_uuid
+        }
+        db.collection(building_label).document(room_uuid).set(data)
+
+
+
+        #UNSAVED_ROOMS.pop(room_uuid)
         return 'OK'
 
 @APP.route('/clasify', methods=['POST'])
