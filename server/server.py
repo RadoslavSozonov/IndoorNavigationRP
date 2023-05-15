1e64a69456d4587b092d8815dfefb3a9e62b9907
Filip Bilinski <F.K.Bilinski@student.tudelft.nl>
2023-05-15 12:59:23 +0200

    Server code clean up

--- server/server.py
+++ server/server.py
@@ -1,5 +1,3 @@
-from room import Room
-from uuid import uuid4
 from flask import Flask, request 
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -8,7 +6,6 @@ import numpy as np
 
 
 APP = Flask(__name__)
-UNSAVED_ROOMS = {"1234": Room(None)}
 
 cred = credentials.Certificate('key.json')
 
@@ -19,67 +16,41 @@ db = firestore.client()
 
 @APP.route('/get_rooms', methods=['GET'])
 def get_rooms():
-    #TODO: quesry database for all rooms
     room_list = []
     collections = db.collections()
     for collection in collections:
         for document in collection.list_documents():
             room_list.append(document.get().to_dict())
-    return room_list
 
-@APP.route('/add_room', methods=['POST', 'GET'])
+    return ' '.join(room['room'] for room in room_list)
+
+@APP.route('/add_room', methods=['POST'])
 def add_room():
 
-    if request.method == 'POST':
-        #audio = request.files['audio']
-        #room = Room(audio)
-        #room_uuid = uuid4()
-        #UNSAVED_ROOMS.update({room_uuid: room})
-        room_data = request.json
-        room_label = room_data['room_label']
-        building_label = room_data['building_label']
-        room_audio = room_data['audio']
-        
-        
-        update_time, doc_ref = db.collection(building_label).add({})
-        data = {
-            u'building': building_label,
-            u'room': room_label,
-            u'audio': "Currently stored locally",
-            u'uuid':doc_ref.id
-        }
-        filename = doc_ref.id + ".wav"
-        concatenatedAudio = sum(room_audio, [])
-        arr = np.asarray(concatenatedAudio)
-        print(arr)
-        write(filename, 44100, arr.astype(np.int16))
+    room_data = request.json
+    room_label = room_data['room_label']
+    building_label = room_data['building_label']
+    room_audio = room_data['audio']
+    
+    
+    update_time, doc_ref = db.collection(building_label).add({})
+    data = {
+        u'building': building_label,
+        u'room': room_label,
+        u'audio': "Currently stored locally",
+        u'uuid':doc_ref.id
+    }
+    doc_ref.update(data)
+
+    filename = doc_ref.id + ".wav"
+    concatenatedAudio = sum(room_audio, [])
+    arr = np.asarray(concatenatedAudio)
+    print(arr)
+    write(filename, 44100, arr.astype(np.int16))
 
 
-        return 'OK'
-        #return room_uuid
+    return 'OK'
 
-    if request.method == 'GET':
-        room_data = request.json
-        room_uuid = room_data['uuid']
-        room_label = room_data['room_label']
-        building_label = room_data['building_label']
-        #room = UNSAVED_ROOMS[room_uuid]
-        #room.set_room_label(room_label)
-        #room.set_building_label(building_label)
-
-        #TODO: save new room to the database
-        data = {
-            u'building': building_label,
-            u'room': room_label,
-            u'audio': "Work in progress, will probably be a URL to firebase storage OR audio file encoding",
-            u'uuid':room_uuid
-        }
-        db.collection(building_label).document(room_uuid).set(data)
-
-
-
-        #UNSAVED_ROOMS.pop(room_uuid)
-        return 'OK'
 
 @APP.route('/clasify', methods=['POST'])
 def calsify_room():
